23b67a5cb9021f3fd7b18d12790dde0aaac99ca6
Yuliana <yulianavidal198@gmail.com>
2024-11-24 13:20:21 -0500

    Cambios el archivo config

--- BackendPlural/src/config.js
+++ BackendPlural/src/config.js
@@ -1,11 +1,18 @@
 import {config} from "dotenv";
 config();
-export const PORT = "https://backend-equipo4.onrender.com";
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+/*export const PORT = "https://backend-equipo4.onrender.com";
 export const HOST = "http://localhost:" + PORT;
 
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
-export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
+export const PAYPAL_API = "https://api-m.sandbox.paypal.com";*/
 
 
 
