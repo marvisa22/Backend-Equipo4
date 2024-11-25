b851ef6afd26190b40862548b76eb2582c61f286
Yuliana <yulianavidal198@gmail.com>
2024-11-24 19:33:51 -0500

    Cambios

--- BackendPlural/src/config.js
+++ BackendPlural/src/config.js
@@ -1,18 +1,13 @@
-import {config} from "dotenv";
+import { config } from "dotenv";
 config();
 
-const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
-/*export const PORT = "https://backend-equipo4.onrender.com";
 export const HOST = "http://localhost:" + PORT;
 
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
-export const PAYPAL_API = "https://api-m.sandbox.paypal.com";*/
+export const PAYPAL_API = "https://api-m.sandbox.paypal.com";
 
 
 
--- BackendPlural/src/index.js
+++ BackendPlural/src/index.js
@@ -1,13 +1,28 @@
 import express from "express";
 import paymentRoutes from "./routes/payment.routes.js";
-import {PORT} from './config.js';
+import { PORT } from './config.js';
 import path from 'path';
 import cors from "cors";
 
 const app = express();
-app.use(cors());
+
+// Configuración de CORS
+app.use(cors({
+    origin: 'https://frontend-equipo4.onrender.com', // URL de tu frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
+// Middleware para manejar JSON
+app.use(express.json());
+
+// Rutas
 app.use(paymentRoutes);
+
+// Archivos estáticos
 app.use(express.static(path.resolve('src/public')));
 
-app.listen(PORT);
-console.log('Server on port', PORT);
+// Inicializar el servidor
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
